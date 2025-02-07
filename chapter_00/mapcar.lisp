f91ed03c64a6c2338fd41c540f1248c4e952723b
Ankit <ankithooda23@gmail.com>
2025-02-07 22:56:51 +0530

    Intro with defparameter

--- chapter_00/mapcar.lisp
+++ chapter_00/mapcar.lisp
@@ -1,10 +1,24 @@
+(setf n1 `(Mr Ankit Hooda))
+(setf n2 `(Reverend Jack Doe))
+(setf n3 `(General John Hancock))
+(setf names (list n1 n2 n3))
+
+(defparameter *titles*
+  `(Mr Mrs Miss Admiral General)
+  "Commonly used titles"
+  )
+
 (defun lastname (name)
   "Get last name from names"
   (first (rest name)))
 
-(setf n1 `(Ankit Hooda))
-(setf n2 `(John Doe))
-(setf n3 `(John Hancock))
-(setf names (list n1 n2 n3))
+(defun firstname (name)
+  (if (member (first name) *titles*)
+      (firstname (rest name))
+      (first name)))
 
 (mapcar #'lastname names)
+
+(mapcar #'first names)
+
+(mapcar #'firstname names)
